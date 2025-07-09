9656e0edf0113ee0ebe719b30c9a6a838a48a704
Artis Vijups <artis@vijups.eu>
2025-07-09 21:37:06 +0300

    Adjusted label placement

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752081262',resources: [
+    cacheName: 'chirpy-1752085988',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- lv/assets/js/data/swconf.js
+++ lv/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752081264',resources: [
+    cacheName: 'chirpy-1752085996',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
