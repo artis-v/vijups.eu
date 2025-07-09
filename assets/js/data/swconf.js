ecc98b6f4519df56b562a4905fdcd37e04cc0308
Artis Vijups <artis@vijups.eu>
2025-07-09 20:14:45 +0300

    Mistyped title

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752080782',resources: [
+    cacheName: 'chirpy-1752081262',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- lv/assets/js/data/swconf.js
+++ lv/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752080785',resources: [
+    cacheName: 'chirpy-1752081264',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
