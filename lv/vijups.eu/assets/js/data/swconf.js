318d7dadd4fa49e42f503a585308965b1a951e3a
Artis Vijups <artis@vijups.eu>
2025-07-09 20:10:04 +0300

    Revamp

---
+++ lv/vijups.eu/assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1752080782',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
