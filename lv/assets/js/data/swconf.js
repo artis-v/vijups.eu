330f8a5f605d7d3330ebf6146627d12e887a1c04
Artis Vijups <artis@vijups.eu>
2025-07-11 03:02:42 +0300

    Polynomial division

--- lv/assets/js/data/swconf.js
+++ lv/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752085996',resources: [
+    cacheName: 'chirpy-1752191965',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
