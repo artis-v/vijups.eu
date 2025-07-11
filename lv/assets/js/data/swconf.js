3f2742372e168659cdbb70d1b7d5693deb0ec30e
Artis Vijups <artis@vijups.eu>
2025-07-11 17:18:04 +0300

    Cauchy

--- lv/assets/js/data/swconf.js
+++ lv/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752192858',resources: [
+    cacheName: 'chirpy-1752243474',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
