2f6357eff0a76362df0d2bbd39dcece1d0ab18d1
Artis Vijups <artis@vijups.eu>
2025-07-19 16:37:16 +0300

    Faulhaber

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752332723',resources: [
+    cacheName: 'chirpy-1752932156',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
